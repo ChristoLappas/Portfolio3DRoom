91905560e68ca36fdfa86f0b29336eb29ca039be
ChristoLappas <christos.lappas@student.pxl.be>
2025-05-22 13:59:36 +0200

    add explorer icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import "xp.css/dist/XP.css";
 
@@ -8,16 +6,7 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+    <>      
       <div style={{ width: 300 }} className="window">
       <div className="title-bar">
         <div className="title-bar-text">Counter</div>
